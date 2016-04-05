919a6fe6d08af08369fdd0badaaf2f7a10a2399f
m.v.novikov88 <m.v.novikov88@gmail.com>
2016-04-05 22:53:27 +0300

    start develop content part

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 
 $(function(){
     console.log('main.js ready');
+    var banner_swiper = new Swiper('.main-banners.swiper-container', {
+        speed: 400,
+        nextButton: '.banners-swiper-next',
+        prevButton: '.banners-swiper-prev'
+    });
 });
