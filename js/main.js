fc6a7158d57119220e774a8b669af61d310afb5f
m.v.novikov88 <m.v.novikov88@gmail.com>
2016-04-06 22:22:17 +0300

    blocks width products card, and some img

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ $(function(){
         nextButton: '.banners-swiper-next',
         prevButton: '.banners-swiper-prev'
     });
+    var best_product_swiper = new Swiper('.best-products.swiper-container', {
+        speed: 400,
+        slidesPerView: 'auto',
+        nextButton: '.best-products-swiper-next',
+        prevButton: '.best-products-swiper-prev'
+    });
+    var best_product_swiper = new Swiper('.new-products.swiper-container', {
+        speed: 400,
+        slidesPerView: 'auto',
+        nextButton: '.new-products-swiper-next',
+        prevButton: '.new-products-swiper-prev'
+    });
 });
