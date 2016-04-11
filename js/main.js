e6f38da220931e5dcfcf5db23c6597ba8c6fe1f3
m.v.novikov88 <m.v.novikov88@gmail.com>
2016-04-11 21:12:30 +0300

    this is the end of test's

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 
 $(function(){
     console.log('main.js ready');
+
+    $('.guantity-but').on('click', function(e){
+        var quantity = $(this).closest('.length'),
+            plus = $('.plus-but', quantity),
+            minus = $('.minus-but', quantity),
+            num_quantity = $('.num', quantity),
+            num = parseInt(num_quantity.attr('data-value')),
+            current_target =  $(e.target);
+        if(current_target.is(plus)){
+            num = num + 1;
+            num_quantity.attr('data-value', num);
+            num_quantity.html(num)
+        }else if(current_target.is(minus)){
+            num = num - 1;
+            num_quantity.attr('data-value', num);
+            num_quantity.html(num)
+        }
+    });
+
+
     var banner_swiper = new Swiper('.main-banners.swiper-container', {
         speed: 400,
         nextButton: '.banners-swiper-next',
